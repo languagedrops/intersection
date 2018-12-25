02301890b96204057a6bb72d793b52ed63a32544
Lucas <lucas@languagedrops.com>
2018-12-25 16:04:07 +0100

    feature(UserUpdate): move out basic user types

--- dist/version-4/index.js
+++ dist/version-4/index.js
@@ -5,4 +5,3 @@ function __export(m) {
 Object.defineProperty(exports, "__esModule", { value: true });
 __export(require("./purchase"));
 __export(require("./errors"));
-__export(require("./settings-sync"));
