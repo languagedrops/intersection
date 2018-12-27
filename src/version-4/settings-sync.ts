8bc8ea181c7d9db70330f4859c3566c43ee34d1d
Lucas <lucas@languagedrops.com>
2018-12-27 16:32:48 +0100

    reafctor(SettingsSync): remove consent

--- src/version-4/settings-sync.ts
+++ src/version-4/settings-sync.ts
@@ -7,7 +7,6 @@ interface Dictionary<T> { readonly [key: string]: T }
 
 export interface PayloadUserSettingsV4 {
   readonly deviceLanguage?: PropertyWithMetadata<string>
-  readonly consent?: PropertyWithMetadata<Dictionary<boolean>>
 }
 
 export interface PayloadUserAppDataV4 {
