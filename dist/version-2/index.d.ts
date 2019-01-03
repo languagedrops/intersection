82c832f8f4c1533c908ca796b039fbf4247c0404
Lucas <lucas@languagedrops.com>
2019-01-03 10:44:30 +0100

    feature(VerifyIAP): add payloads

--- dist/version-2/index.d.ts
+++ dist/version-2/index.d.ts
@@ -1,2 +1,3 @@
 export * from './user-update';
 export * from './purchase';
+export * from './verify-iap';
---
+++ dist/version-2/verify-iap.d.ts
@@ -0,0 +1,16 @@
+import { PayloadAndroidReceiptV2 } from './purchase';
+export interface PayloadVerifyIAPRequestV2 {
+    readonly productId: string;
+    readonly receipt: string | PayloadAndroidReceiptV2;
+}
+interface PayloadVerifyIAPValidResponseV2 {
+    readonly isValid: true;
+    readonly productId: string;
+}
+interface PayloadVerifyIAPInvalidResponseV2 {
+    readonly isValid: false;
+    readonly status?: string;
+    readonly productId: string;
+}
+export declare type PayloadVerifyIAPResponseV2 = PayloadVerifyIAPValidResponseV2 | PayloadVerifyIAPInvalidResponseV2;
+export {};
