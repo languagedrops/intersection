e7f1233d638455a04755005b82c8390947e4b5ab
Daniel Falus <daniel.falus@languagedrops.com>
2019-01-11 14:03:41 +0100

    feature(ContentReporting) - add payload type

---
+++ dist/version-4/content-reporting-save.d.ts
@@ -0,0 +1,16 @@
+export interface PayloadContentReportV4 {
+    readonly nativeLanguage: string;
+    readonly learningLanguage: string;
+    readonly wordId: string;
+    readonly soundId: string;
+    readonly cliendUserId: string;
+    readonly email: string;
+    readonly appVersion: string;
+    readonly platform: string;
+    readonly osVersion: string;
+    readonly appType: string;
+    readonly comment: string;
+    readonly contentVersion: string;
+    readonly soundVersion: string;
+    readonly translationVersion: string;
+}
