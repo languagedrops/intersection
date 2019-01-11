ec1e794228cf11bc7f24f917d4775db8eaaf46a6
Daniel Falus <daniel.falus@languagedrops.com>
2019-01-11 14:47:22 +0100

    feature(ContentReporting) - add issue type to payload

--- src/version-4/content-reporting-save.ts
+++ src/version-4/content-reporting-save.ts
@@ -1,16 +1,21 @@
-export interface PayloadContentReportV4 {
+export interface PayloadContentReportMetadataRequestV4 {
   readonly nativeLanguage: string
   readonly learningLanguage: string
   readonly wordId: string
-  readonly cliendUserId: string
-  readonly email: string
   readonly appVersion: string
   readonly platform: string
   readonly osVersion: string
   readonly appType: string
-  readonly comment: string
   readonly contentVersion: string
   readonly soundVersion: string
   readonly romanization: string
   readonly alternativeWriting: string
+}
+
+export interface PayloadContentReportRequestV4 {
+  readonly metadata: PayloadContentReportMetadataRequestV4
+  readonly cliendUserId: string
+  readonly email: string
+  readonly comment: string
+  readonly issueType: string
 }
