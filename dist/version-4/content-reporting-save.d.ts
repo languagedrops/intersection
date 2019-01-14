cf5b8a5a334bed299c4bd72f8afd1f1ec4a9b647
Daniel Falus <daniel.falus@languagedrops.com>
2019-01-14 11:18:00 +0100

    feature(ContentReporting) - add payload type (#29)
    * feature(ContentReporting) - add payload type
    
    * feature(ContentReporting) - replace some properties
    
    * feature(ContentReporting) - add issue type to payload
    
    * feature(ContentReporting) - dont split payload
    
    * feature(ContentReporting) - create enum for issue types
    
    * feature(ContentReport) - rename enum

---
+++ dist/version-4/content-reporting-save.d.ts
@@ -0,0 +1,23 @@
+export declare enum PayloadContentReportIssueType {
+    Misspelling = "Misspelling",
+    Translation = "Translation",
+    Illustration = "Illustration",
+    Pronunciation = "Pronunciation"
+}
+export interface PayloadContentReportRequestV4 {
+    readonly nativeLanguage: string;
+    readonly learningLanguage: string;
+    readonly appVersion: string;
+    readonly platform: string;
+    readonly osVersion: string;
+    readonly appType: string;
+    readonly contentVersion: string;
+    readonly soundVersion: string;
+    readonly romanization: string;
+    readonly alternativeWriting: string;
+    readonly wordId: string;
+    readonly cliendUserId: string;
+    readonly email: string;
+    readonly comment: string;
+    readonly issueTypes: PayloadContentReportIssueType[];
+}
---
+++ dist/version-4/content-reporting-save.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var PayloadContentReportIssueType;
+(function (PayloadContentReportIssueType) {
+    PayloadContentReportIssueType["Misspelling"] = "Misspelling";
+    PayloadContentReportIssueType["Translation"] = "Translation";
+    PayloadContentReportIssueType["Illustration"] = "Illustration";
+    PayloadContentReportIssueType["Pronunciation"] = "Pronunciation";
+})(PayloadContentReportIssueType = exports.PayloadContentReportIssueType || (exports.PayloadContentReportIssueType = {}));
