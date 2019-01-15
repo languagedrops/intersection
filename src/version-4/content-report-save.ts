1faddfe4d9cf3fd74b7854f34ff09bb4efa7873f
Daniel Falus <daniel.falus@languagedrops.com>
2019-01-15 11:29:09 +0100

    feature(ContentReporting) - add translation version (#32)
    * feature(ContentReporting) - rename property
    
    * feature(ContentReport) - keep content version as well

--- src/version-4/content-report-save.ts
+++ src/version-4/content-report-save.ts
@@ -12,6 +12,7 @@ export interface PayloadContentReportRequestV4 {
   readonly platform: string
   readonly osVersion: string
   readonly appType: string
+  readonly translationVersion: string
   readonly contentVersion: string
   readonly soundVersion: string
   readonly romanization: string
