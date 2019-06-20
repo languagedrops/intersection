891cd697abb3440725a65cf8fc6c3a1e4915d078
Anton Semenov <anta.semenov@gmail.com>
2019-06-20 13:49:09 +0500

    feature(EmailProvider): add types for new endpoint

---
+++ src/version-4/login-provider.ts
@@ -0,0 +1,18 @@
+import { PayloadRequestBaseV4 } from './base'
+import { PayloadLoginTypeV4 } from './user-update'
+
+export interface PayloadProviderForEmailRequestV4 extends PayloadRequestBaseV4 {
+    readonly email: string
+}
+
+export interface PayloadProviderForEmailBaseResponseV4 <T extends boolean> {
+  readonly userExist: T
+}
+
+export interface PayloadProviderForEmailSuccessResponseV4 extends PayloadProviderForEmailBaseResponseV4<true> {
+    readonly provider: PayloadLoginTypeV4
+}
+
+export interface PayloadProviderForEmailNoUserResponseV4 extends PayloadProviderForEmailBaseResponseV4<false> {}
+
+export type PayloadProviderForEmailResponseV4 = PayloadProviderForEmailNoUserResponseV4 | PayloadProviderForEmailSuccessResponseV4
