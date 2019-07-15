560023a9c196aef9bfcea2af5c346825a08b33c5
lucjac <lucas.jacobson@hotmail.de>
2019-07-15 16:57:22 +0200

    fix(Index): export language data (#74)

--- dist/version-4/index.d.ts
+++ dist/version-4/index.d.ts
@@ -8,3 +8,5 @@ export * from './progress';
 export * from './content-report-save';
 export * from './dictionary';
 export * from './login-provider';
+export * from './language';
+export * from './languagesStaticData';
