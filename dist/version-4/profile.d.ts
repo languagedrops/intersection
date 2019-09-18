25e27a06939d39cfcba99a187e1b1a03c8a80b41
Anton Semenov <anta.semenov@gmail.com>
2019-09-18 11:35:46 +0200

    feature(ProfileAPI): use separate endpoints for creation and updating profiles

--- dist/version-4/profile.d.ts
+++ dist/version-4/profile.d.ts
@@ -1,12 +1,17 @@
 import { PayloadRequestBaseV4 } from './base';
-export interface PayloadSaveProfileRequestV4 extends PayloadRequestBaseV4 {
-    readonly name: string;
-    readonly profileId?: string;
-}
 export interface PayloadProfileData {
     readonly name: string;
     readonly profileId: string;
 }
+export interface PayloadSaveProfileRequestV4 extends PayloadRequestBaseV4 {
+    readonly name: string;
+    readonly profileId: string;
+}
+export declare type PayloadSaveProfileResponseV4 = PayloadProfileData;
+export interface PayloadCreateProfileRequestV4 extends PayloadRequestBaseV4 {
+    readonly name: string;
+}
+export declare type PayloadCreateProfileResponseV4 = PayloadProfileData;
 export interface PayloadGetProfilesResponseV4 {
     readonly profiles: PayloadProfileData[];
 }
--- src/version-4/profile.ts
+++ src/version-4/profile.ts
@@ -1,14 +1,22 @@
 import { PayloadRequestBaseV4 } from './base'
 
-export interface PayloadSaveProfileRequestV4 extends PayloadRequestBaseV4 {
-  readonly name: string
-  readonly profileId?: string // empty value means we need to create profile
-}
-
 export interface PayloadProfileData {
   readonly name: string
   readonly profileId: string
 }
+
+export interface PayloadSaveProfileRequestV4 extends PayloadRequestBaseV4 {
+  readonly name: string
+  readonly profileId: string
+}
+
+export type PayloadSaveProfileResponseV4 = PayloadProfileData
+
+export interface PayloadCreateProfileRequestV4 extends PayloadRequestBaseV4 {
+  readonly name: string
+}
+
+export type PayloadCreateProfileResponseV4 = PayloadProfileData
 
 export interface PayloadGetProfilesResponseV4 {
   readonly profiles: PayloadProfileData[]
