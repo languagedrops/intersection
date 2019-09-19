02b345f1557f9b9f166a6f63b0eaeeea191cfc27
Anton Semenov <anta.semenov@gmail.com>
2019-09-19 16:15:51 +0200

    feature(DeprecationAPI): reuse appPlatfrom type

--- src/version-4/deprecated-versions.ts
+++ src/version-4/deprecated-versions.ts
@@ -1,7 +1,9 @@
+import { AppPlatform } from './appPlatform'
+
 export interface PayloadGetDeprecatedStatusRequestV4 {
   readonly version: string
   readonly appType: string
-  readonly platform: string
+  readonly platform: AppPlatform
   readonly osVersion: string
   readonly manufacturer: string
 }
