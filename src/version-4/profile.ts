5505320aeb3bec0f1a3212f464630f674bc934a6
Lucas <lucas.jacobson@hotmail.de>
2019-10-17 13:28:47 +0200

    feature(Profile): rename to firstName add lastName

--- src/version-4/profile.ts
+++ src/version-4/profile.ts
@@ -1,19 +1,21 @@
 import { PayloadRequestBaseV4 } from './base'
 
 export interface PayloadProfileData {
-  readonly name: string
+  readonly firstName: string
+  readonly lastName: string
   readonly profileId: string
 }
 
 export interface PayloadSaveProfileRequestV4 extends PayloadRequestBaseV4 {
-  readonly name: string
+  readonly firstName: string
+  readonly lastName: string
   readonly profileId: string
 }
 
 export type PayloadSaveProfileResponseV4 = PayloadProfileData
 
 export interface PayloadCreateProfileRequestV4 extends PayloadRequestBaseV4 {
-  readonly name: string
+  readonly firstName: string
 }
 
 export type PayloadCreateProfileResponseV4 = PayloadProfileData
