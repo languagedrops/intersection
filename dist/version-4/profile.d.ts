9bcfc133d4b283d330d9a27abd951435888cff52
Lucas <lucas.jacobson@hotmail.de>
2019-10-17 13:29:16 +0200

    feature(Profile): build source

--- dist/version-4/profile.d.ts
+++ dist/version-4/profile.d.ts
@@ -1,15 +1,17 @@
 import { PayloadRequestBaseV4 } from './base';
 export interface PayloadProfileData {
-    readonly name: string;
+    readonly firstName: string;
+    readonly lastName: string;
     readonly profileId: string;
 }
 export interface PayloadSaveProfileRequestV4 extends PayloadRequestBaseV4 {
-    readonly name: string;
+    readonly firstName: string;
+    readonly lastName: string;
     readonly profileId: string;
 }
 export declare type PayloadSaveProfileResponseV4 = PayloadProfileData;
 export interface PayloadCreateProfileRequestV4 extends PayloadRequestBaseV4 {
-    readonly name: string;
+    readonly firstName: string;
 }
 export declare type PayloadCreateProfileResponseV4 = PayloadProfileData;
 export interface PayloadGetProfilesResponseV4 {
