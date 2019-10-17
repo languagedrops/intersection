d37e86eba8549ca337cffb5007fd45b0032eb11b
Lucas <lucas.jacobson@hotmail.de>
2019-10-17 14:13:44 +0200

    fix(Profile): make lastName optional

--- dist/version-4/profile.d.ts
+++ dist/version-4/profile.d.ts
@@ -1,12 +1,12 @@
 import { PayloadRequestBaseV4 } from './base';
 export interface PayloadProfileData {
     readonly firstName: string;
-    readonly lastName: string;
+    readonly lastName?: string;
     readonly profileId: string;
 }
 export interface PayloadSaveProfileRequestV4 extends PayloadRequestBaseV4 {
     readonly firstName: string;
-    readonly lastName: string;
+    readonly lastName?: string;
     readonly profileId: string;
 }
 export declare type PayloadSaveProfileResponseV4 = PayloadProfileData;
