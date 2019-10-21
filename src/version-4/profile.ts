8542db1498f4b0681d8f9c01f6ee3f46387cac95
lucjac <lucas.jacobson@hotmail.de>
2019-10-21 09:13:48 +0200

    chore(Profile): rename to update (#95)

--- src/version-4/profile.ts
+++ src/version-4/profile.ts
@@ -6,13 +6,13 @@ export interface PayloadProfileData {
   readonly profileId: string
 }
 
-export interface PayloadSaveProfileRequestV4 extends PayloadRequestBaseV4 {
+export interface PayloadUpdateProfileRequestV4 extends PayloadRequestBaseV4 {
   readonly firstName: string
   readonly lastName?: string
   readonly profileId: string
 }
 
-export type PayloadSaveProfileResponseV4 = PayloadProfileData
+export type PayloadUpdateProfileResponseV4 = PayloadProfileData
 
 export interface PayloadCreateProfileRequestV4 extends PayloadRequestBaseV4 {
   readonly firstName: string
