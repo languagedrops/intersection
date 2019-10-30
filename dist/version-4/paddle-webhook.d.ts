9afdfd19c5c380defd961eb681fae5e092510c99
Lucas <lucas.jacobson@hotmail.de>
2019-10-30 14:35:55 +0100

    feature(Paddle): add premium periods

--- dist/version-4/paddle-webhook.d.ts
+++ dist/version-4/paddle-webhook.d.ts
@@ -1,7 +1,8 @@
+declare type GiftingPremiumPeriod = 'month' | 'year' | 'lifetime' | '3months';
 interface PaddlePassthroughDataGifting {
     readonly gifterEmail: string;
     readonly receiverName: string;
-    readonly premiumPeriod: string;
+    readonly premiumPeriod: GiftingPremiumPeriod;
 }
 export interface PaddlePassthroughObjectGifting {
     readonly passthrough: PaddlePassthroughDataGifting;
--- src/version-4/paddle-webhook.ts
+++ src/version-4/paddle-webhook.ts
@@ -1,7 +1,9 @@
+type GiftingPremiumPeriod = 'month' | 'year' | 'lifetime' | '3months'
+
 interface PaddlePassthroughDataGifting {
     readonly gifterEmail: string,
     readonly receiverName: string,
-    readonly premiumPeriod: string,
+    readonly premiumPeriod: GiftingPremiumPeriod,
 }
 
 export interface PaddlePassthroughObjectGifting {
