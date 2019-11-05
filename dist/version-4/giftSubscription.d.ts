3538b7cc98636765b03d0497dff3fcf976821acd
Anton Semenov <anta.semenov@gmail.com>
2019-11-05 16:58:31 +0300

    feature(GiftSubscription): add API for redeemCode endpoint

---
+++ dist/version-4/giftSubscription.d.ts
@@ -0,0 +1,17 @@
+import { PayloadRequestBaseV4 } from './base';
+export interface PayloadGiftSubscriptionRedeemCodeRequestV4 extends PayloadRequestBaseV4 {
+    readonly couponCode: string;
+}
+export declare enum PayloaddGiftSubscriptionRedeemCodeStatusV4 {
+    Success = "success",
+    Expired = "expired",
+    Error = "error"
+}
+interface PayloadGiftSubscriptionRedeemCodeResponseBase<T extends PayloaddGiftSubscriptionRedeemCodeStatusV4> {
+    readonly status: T;
+}
+export interface PayloadGiftSubscriptionRedeemCodeErrorV4 extends PayloadGiftSubscriptionRedeemCodeResponseBase<PayloaddGiftSubscriptionRedeemCodeStatusV4.Error> {
+    readonly errorCode: string;
+}
+export declare type PayloadGiftSubscriptionRedeemCodeResponseV4 = PayloadGiftSubscriptionRedeemCodeErrorV4 | PayloadGiftSubscriptionRedeemCodeResponseBase<PayloaddGiftSubscriptionRedeemCodeStatusV4.Success> | PayloadGiftSubscriptionRedeemCodeResponseBase<PayloaddGiftSubscriptionRedeemCodeStatusV4.Expired>;
+export {};
