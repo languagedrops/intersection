e7948e7fa674fe724c73841368da9e953eb2de85
Anton Semenov <anta.semenov@gmail.com>
2019-11-06 13:34:58 +0300

    feature(GiftSubscription): add API for redeemCode endpoint (#102)
    * feature(GiftSubscription): add API for redeemCode endpoint
    
    * feature(GiftSubscription): add gift subscription to index
    
    * feature(GiftAPI): reduce number of coupon statuses

---
+++ dist/version-4/giftSubscription.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var PayloaddGiftSubscriptionRedeemCodeStatusV4;
+(function (PayloaddGiftSubscriptionRedeemCodeStatusV4) {
+    PayloaddGiftSubscriptionRedeemCodeStatusV4["Success"] = "success";
+    PayloaddGiftSubscriptionRedeemCodeStatusV4["Error"] = "error";
+})(PayloaddGiftSubscriptionRedeemCodeStatusV4 = exports.PayloaddGiftSubscriptionRedeemCodeStatusV4 || (exports.PayloaddGiftSubscriptionRedeemCodeStatusV4 = {}));
---
+++ src/version-4/giftSubscription.ts
@@ -0,0 +1,21 @@
+import { PayloadRequestBaseV4 } from './base'
+
+export interface PayloadGiftSubscriptionRedeemCodeRequestV4 extends PayloadRequestBaseV4 {
+  readonly couponCode: string
+}
+
+export enum PayloaddGiftSubscriptionRedeemCodeStatusV4 {
+  Success = 'success',
+  Error = 'error',
+}
+
+interface PayloadGiftSubscriptionRedeemCodeResponseBase<T extends PayloaddGiftSubscriptionRedeemCodeStatusV4> {
+  readonly status: T
+}
+
+export interface PayloadGiftSubscriptionRedeemCodeErrorV4 extends PayloadGiftSubscriptionRedeemCodeResponseBase<PayloaddGiftSubscriptionRedeemCodeStatusV4.Error> {
+  readonly errorCode: string
+}
+
+export type PayloadGiftSubscriptionRedeemCodeResponseV4 = PayloadGiftSubscriptionRedeemCodeErrorV4
+| PayloadGiftSubscriptionRedeemCodeResponseBase<PayloaddGiftSubscriptionRedeemCodeStatusV4.Success>
