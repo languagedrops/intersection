8e502789661058d5425926f0db4134013b7d894b
Lucas <lucas.jacobson@hotmail.de>
2019-11-08 12:31:38 +0100

    feature(Paddle): remove unneeded passthrough

--- dist/version-4/paddle-webhook.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var GiftPeriod;
-(function (GiftPeriod) {
-    GiftPeriod["Month"] = "month";
-    GiftPeriod["Year"] = "year";
-    GiftPeriod["Lifetime"] = "lifetime";
-})(GiftPeriod = exports.GiftPeriod || (exports.GiftPeriod = {}));
