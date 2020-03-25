4260bfd9b29139fc7a4fe717ce39f7143af779f3
lucjac <lucas.jacobson@hotmail.de>
2020-03-25 15:55:44 +0100

    feature(PaddleWebhook): add 3 months coupons (#129)

--- dist/version-4/paddle-webhook.d.ts
+++ dist/version-4/paddle-webhook.d.ts
@@ -1,5 +1,6 @@
 export declare enum GiftPeriod {
     Month = "month",
+    ThreeMonth = "threeMonth",
     Year = "year",
     Lifetime = "lifetime"
 }
--- dist/version-4/paddle-webhook.js
+++ dist/version-4/paddle-webhook.js
@@ -3,6 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var GiftPeriod;
 (function (GiftPeriod) {
     GiftPeriod["Month"] = "month";
+    GiftPeriod["ThreeMonth"] = "threeMonth";
     GiftPeriod["Year"] = "year";
     GiftPeriod["Lifetime"] = "lifetime";
 })(GiftPeriod = exports.GiftPeriod || (exports.GiftPeriod = {}));
