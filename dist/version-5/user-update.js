e511e3ded114c8551fd92e422a4c6e96ef014170
Anton Semenov <anta.semenov@gmail.com>
2020-03-27 17:14:52 +0500

    feature(AppleLogin): add userId, make email optional (#130)
    * feature(AppleLogin): add userId, make email optional
    
    * feature(AppleLogin): introduce v5 of types
    
    * feature(AppleSignIn): build intersection

---
+++ dist/version-5/user-update.js
@@ -0,0 +1,14 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var PayloadLoginTypeV5;
+(function (PayloadLoginTypeV5) {
+    PayloadLoginTypeV5["Google"] = "Google";
+    PayloadLoginTypeV5["Facebook"] = "Facebook";
+    PayloadLoginTypeV5["Email"] = "Email";
+    PayloadLoginTypeV5["Apple"] = "Apple";
+})(PayloadLoginTypeV5 = exports.PayloadLoginTypeV5 || (exports.PayloadLoginTypeV5 = {}));
+var PayloadAppPlatformSendUserDetailsRequestV5;
+(function (PayloadAppPlatformSendUserDetailsRequestV5) {
+    PayloadAppPlatformSendUserDetailsRequestV5["iOS"] = "iOS";
+    PayloadAppPlatformSendUserDetailsRequestV5["Android"] = "Android";
+})(PayloadAppPlatformSendUserDetailsRequestV5 = exports.PayloadAppPlatformSendUserDetailsRequestV5 || (exports.PayloadAppPlatformSendUserDetailsRequestV5 = {}));
