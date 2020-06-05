c13c721f071c5ec414d8b5791f96167c10077911
lucjac <lucas.jacobson@hotmail.de>
2020-06-05 14:17:27 +0200

    feature(LoginProvider): revert V4 and add V5 (#151)

--- dist/version-4/login-provider.d.ts
+++ dist/version-4/login-provider.d.ts
@@ -3,20 +3,12 @@ import { PayloadLoginTypeV4 } from './user-update';
 export interface PayloadProviderForEmailRequestV4 extends PayloadRequestBaseV4 {
     readonly email: string;
 }
-export interface PayloadProviderForAppleIdRequestV4 extends PayloadRequestBaseV4 {
-    readonly appleId: string;
-}
-export interface PayloadProviderForCognitoIdRequestV4 extends PayloadRequestBaseV4 {
-    readonly cognitoId: string;
-}
-export interface PayloadProviderForLoginBaseResponseV4<T extends boolean> {
+export interface PayloadProviderForEmailBaseResponseV4<T extends boolean> {
     readonly userExist: T;
 }
-export interface PayloadProviderForLoginSuccessResponseV4 extends PayloadProviderForLoginBaseResponseV4<true> {
+export interface PayloadProviderForEmailSuccessResponseV4 extends PayloadProviderForEmailBaseResponseV4<true> {
     readonly provider: PayloadLoginTypeV4;
-    readonly migratedToFirebase: boolean;
 }
-export interface PayloadProviderForLoginNoUserResponseV4 extends PayloadProviderForLoginBaseResponseV4<false> {
+export interface PayloadProviderForEmailNoUserResponseV4 extends PayloadProviderForEmailBaseResponseV4<false> {
 }
-export declare type PayloadProviderForLoginResponseV4 = PayloadProviderForLoginNoUserResponseV4 | PayloadProviderForLoginSuccessResponseV4;
-export declare type PayloadProviderForLoginRequestV4 = PayloadProviderForEmailRequestV4 | PayloadProviderForAppleIdRequestV4 | PayloadProviderForCognitoIdRequestV4;
+export declare type PayloadProviderForEmailResponseV4 = PayloadProviderForEmailNoUserResponseV4 | PayloadProviderForEmailSuccessResponseV4;
--- src/version-5/index.ts
+++ src/version-5/index.ts
@@ -1,1 +1,2 @@
+export * from './login-provider'
 export * from './user-update'
