85d1ae5fd0e39784f21e7eb4a044e23919c6b6a3
lucjac <lucas.jacobson@hotmail.de>
2020-07-01 13:08:16 +0200

    feature(SendEvents): add sendEvents (#159)

---
+++ src/version-5/send-events.ts
@@ -0,0 +1,16 @@
+import { PayloadIAPIdentifierV4 } from '../version-4'
+
+export enum SendEventsTypes {
+    CartAbandoned = 'CartAbandoned',
+}
+
+export interface PayloadSendEventsCartAbandonedRequestV5 {
+    readonly type: SendEventsTypes.CartAbandoned
+    readonly topUpId: PayloadIAPIdentifierV4
+}
+
+export type PayloadSendEventsRequestV5 = PayloadSendEventsCartAbandonedRequestV5
+
+export interface PayloadSendEventsResponseV5 {
+    readonly success: boolean
+}
