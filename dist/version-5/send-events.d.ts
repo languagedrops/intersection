ade900286b86944145d7ac08e127d9c24a190f32
Lucas <lucas.jacobson@hotmail.de>
2020-07-07 10:58:58 +0200

    chore(Project): generate JS files

--- dist/version-5/send-events.d.ts
+++ dist/version-5/send-events.d.ts
@@ -1,10 +1,12 @@
 import { PayloadIAPIdentifierV4 } from '../version-4';
+import { AppPlatform } from '../appPlatform';
 export declare enum SendEventsTypes {
     CartAbandoned = "CartAbandoned"
 }
 export interface PayloadSendEventsCartAbandonedRequestV5 {
     readonly type: SendEventsTypes.CartAbandoned;
     readonly topUpId: PayloadIAPIdentifierV4;
+    readonly platform: AppPlatform;
 }
 export declare type PayloadSendEventsRequestV5 = PayloadSendEventsCartAbandonedRequestV5[];
 export interface PayloadSendEventsResponseV5 {
