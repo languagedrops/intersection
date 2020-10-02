335489b45198f229009cd86dc122447ebf959917
Anton Semenov <anta.semenov@gmail.com>
2020-10-02 10:49:57 +0500

    chore(Project): generate JS files

--- dist/version-5/send-events.d.ts
+++ dist/version-5/send-events.d.ts
@@ -1,14 +1,21 @@
 import { PayloadIAPIdentifierV4 } from '../version-4';
 import { AppPlatform } from '../appPlatform';
 export declare enum SendEventsTypes {
-    CartAbandoned = "CartAbandoned"
+    CartAbandoned = "CartAbandoned",
+    PurchaseGiftSubscription = "PurchaseGiftSubscription"
 }
 export interface PayloadSendEventsCartAbandonedRequestV5 {
     readonly type: SendEventsTypes.CartAbandoned;
     readonly topUpId: PayloadIAPIdentifierV4;
     readonly platform: AppPlatform;
 }
-export declare type PayloadSendEventsRequestV5 = PayloadSendEventsCartAbandonedRequestV5[];
+export interface PayloadSendPurchaseGiftSubscriptionRequestV5 {
+    readonly type: SendEventsTypes.PurchaseGiftSubscription;
+    readonly email: string;
+}
+declare type SendEventRequestItem = PayloadSendEventsCartAbandonedRequestV5 | PayloadSendPurchaseGiftSubscriptionRequestV5;
+export declare type PayloadSendEventsRequestV5 = SendEventRequestItem[];
 export interface PayloadSendEventsResponseV5 {
     readonly success: boolean;
 }
+export {};
--- dist/version-5/send-events.js
+++ dist/version-5/send-events.js
@@ -3,4 +3,5 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var SendEventsTypes;
 (function (SendEventsTypes) {
     SendEventsTypes["CartAbandoned"] = "CartAbandoned";
+    SendEventsTypes["PurchaseGiftSubscription"] = "PurchaseGiftSubscription";
 })(SendEventsTypes = exports.SendEventsTypes || (exports.SendEventsTypes = {}));
