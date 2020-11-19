6767e706d119f4691335eb9ebac67a9cb8f15c0f
Rogue Rotkosky <6845222+roguealexander@users.noreply.github.com>
2020-11-19 14:06:35 +0100

    feature(FeatureIntroductionsShown): Add new state property (#187)

--- src/version-4/settings-sync.ts
+++ src/version-4/settings-sync.ts
@@ -57,6 +57,7 @@ export interface PayloadUserAppDataV4 {
   readonly numberOfReActivations?: PropertyWithMetadata<number>
   readonly segmentsCommunication?: PropertyWithMetadata<string>
   readonly userQuestions?: PropertyWithMetadata<Dictionary<string | undefined>>
+  readonly featureIntroductionsShown?: PropertyWithMetadata<Dictionary<number>>
 }
 
 export interface PayloadUserLanguageSettingsV4 {
