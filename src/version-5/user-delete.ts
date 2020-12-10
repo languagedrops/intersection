059ae7f3d764b62678cc84754063852b39311c11
Lucas <lucas.jacobson@hotmail.de>
2020-12-10 14:42:56 +0100

    fix(UserDelete): rename v5

--- src/version-5/user-delete.ts
+++ src/version-5/user-delete.ts
@@ -1,9 +1,9 @@
 import { PayloadRequestBaseV5 } from './base'
 
-export interface PayloadDeleteUserRequestV4 extends PayloadRequestBaseV5 {
+export interface PayloadDeleteUserRequestV5 extends PayloadRequestBaseV5 {
     readonly userId: string
 }
 
-export interface PayloadDeleteUserResponseV4 {
+export interface PayloadDeleteUserResponseV5 {
     readonly success: boolean
 }
