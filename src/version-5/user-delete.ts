d5a420dc76e65e0033d3d1bca8d03b6d31f83790
Lucas <lucas.jacobson@hotmail.de>
2020-12-18 11:04:33 +0100

    refactor(UserDelete): remove userId

--- src/version-5/user-delete.ts
+++ src/version-5/user-delete.ts
@@ -1,8 +1,6 @@
 import { PayloadRequestBaseV5 } from './base'
 
-export interface PayloadDeleteUserRequestV5 extends PayloadRequestBaseV5 {
-    readonly userId: string
-}
+export interface PayloadDeleteUserRequestV5 extends PayloadRequestBaseV5 {}
 
 export interface PayloadDeleteUserResponseV5 {
     readonly success: boolean
