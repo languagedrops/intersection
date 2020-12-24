9b019822b2af5c33c237ab837f328ffa7b905acc
Daria Semenova <daria_semenova_work@gmail.com>
2020-12-24 17:03:47 +0300

    feature(FeatureRequest): add payload for feature suggestion

---
+++ src/version-5/send-feature-suggestion.ts
@@ -0,0 +1,10 @@
+import { PayloadRequestBaseV5 } from './base'
+
+export interface PayloadFeatureSuggestionRequestV5 extends PayloadRequestBaseV5 {
+  readonly suggestion: string
+  readonly email?: string
+}
+
+export interface PayloadFeatureSuggestionRequestV5ResponseV5 {
+  readonly success: boolean
+}
