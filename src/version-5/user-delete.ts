8605c60121a533b25fed1b27aa8f85fe51c0317e
Anton Semenov <anta.semenov@gmail.com>
2020-12-24 18:28:53 +0300

    feature(DeleteUser): make types more tight (#198)

--- src/version-5/user-delete.ts
+++ src/version-5/user-delete.ts
@@ -8,16 +8,28 @@ interface PayloadDeleteUserRequestV5Base<T extends PayloadLoginTypeV5> extends P
 interface PayloadDeleteUserRequestV5Facebook extends PayloadDeleteUserRequestV5Base<PayloadLoginTypeV5.Facebook> {}
 interface PayloadDeleteUserRequestV5Google extends PayloadDeleteUserRequestV5Base<PayloadLoginTypeV5.Google> {}
 
-interface PayloadDeleteUserRequestV5Apple extends PayloadDeleteUserRequestV5Base<PayloadLoginTypeV5.Apple> {
+interface PayloadDeleteUserRequestV5AppleMigrated extends PayloadDeleteUserRequestV5Base<PayloadLoginTypeV5.Apple> {
   readonly firebaseUid: string
-  readonly wasMigrated: boolean
+  readonly wasMigrated: true
 }
-interface PayloadDeleteUserRequestV5Email extends PayloadDeleteUserRequestV5Base<PayloadLoginTypeV5.Email> {
-  readonly firebaseUid: string
-  readonly wasMigrated: boolean
+interface PayloadDeleteUserRequestV5AppleNotMigrated extends PayloadDeleteUserRequestV5Base<PayloadLoginTypeV5.Apple> {
+  readonly wasMigrated: false
 }
 
-export type PayloadDeleteUserRequestV5 = PayloadDeleteUserRequestV5Facebook | PayloadDeleteUserRequestV5Google | PayloadDeleteUserRequestV5Apple | PayloadDeleteUserRequestV5Email
+interface PayloadDeleteUserRequestV5EmailMigrated extends PayloadDeleteUserRequestV5Base<PayloadLoginTypeV5.Email> {
+  readonly firebaseUid: string
+  readonly wasMigrated: true
+}
+interface PayloadDeleteUserRequestV5EmailNotMigrated extends PayloadDeleteUserRequestV5Base<PayloadLoginTypeV5.Email> {
+  readonly wasMigrated: false
+}
+
+export type PayloadDeleteUserRequestV5 = PayloadDeleteUserRequestV5Facebook
+| PayloadDeleteUserRequestV5Google
+| PayloadDeleteUserRequestV5AppleMigrated
+| PayloadDeleteUserRequestV5AppleNotMigrated
+| PayloadDeleteUserRequestV5EmailMigrated
+| PayloadDeleteUserRequestV5EmailNotMigrated
 
 export interface PayloadDeleteUserResponseV5 {
     readonly success: boolean
