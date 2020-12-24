6f938436a67d48cf0932f8480ac5926679de2f86
Daria Semenova <daria_semenova_work@gmail.com>
2020-12-24 17:41:52 +0300

    feature(DeleteUser): add firebase uid for email and apple

--- src/version-5/user-delete.ts
+++ src/version-5/user-delete.ts
@@ -1,6 +1,23 @@
 import { PayloadRequestBaseV5 } from './base'
+import { PayloadLoginTypeV5 } from './user-update'
 
-export interface PayloadDeleteUserRequestV5 extends PayloadRequestBaseV5 {}
+interface PayloadDeleteUserRequestV5Base<T extends PayloadLoginTypeV5> extends PayloadRequestBaseV5 {
+  readonly loginType: T
+}
+
+interface PayloadDeleteUserRequestV5Facebook extends PayloadDeleteUserRequestV5Base<PayloadLoginTypeV5.Facebook> {}
+interface PayloadDeleteUserRequestV5Google extends PayloadDeleteUserRequestV5Base<PayloadLoginTypeV5.Google> {}
+
+interface PayloadDeleteUserRequestV5Apple extends PayloadDeleteUserRequestV5Base<PayloadLoginTypeV5.Apple> {
+  readonly firebaseUid: string
+  readonly wasMigrated: boolean
+}
+interface PayloadDeleteUserRequestV5Email extends PayloadDeleteUserRequestV5Base<PayloadLoginTypeV5.Email> {
+  readonly firebaseUid: string
+  readonly wasMigrated: boolean
+}
+
+export type PayloadDeleteUserRequestV5 = PayloadDeleteUserRequestV5Facebook | PayloadDeleteUserRequestV5Google | PayloadDeleteUserRequestV5Apple | PayloadDeleteUserRequestV5Email
 
 export interface PayloadDeleteUserResponseV5 {
     readonly success: boolean
