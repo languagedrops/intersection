7c65c11d71c0639af86a3723719d2a7f040f90b4
Lucas <lucas.jacobson@hotmail.de>
2021-03-19 13:42:45 +0100

    chore(Project): generate JS files

--- dist/index.js
+++ dist/index.js
@@ -8,6 +8,7 @@ __export(require("./version-4/"));
 __export(require("./version-5/"));
 __export(require("./appPlatform"));
 __export(require("./appType"));
+__export(require("./environment"));
 __export(require("./language"));
 __export(require("./languagesStaticData"));
 __export(require("./productIdTopUpIdMap"));
