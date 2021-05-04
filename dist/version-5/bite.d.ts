30f3eda744532b3f74b2fd670e1a82aeff09daec
Lucas <lucas.jacobson@hotmail.de>
2021-05-04 16:06:00 +0200

    chore(Project): generate JS files

---
+++ dist/version-5/bite.d.ts
@@ -0,0 +1,26 @@
+import { LanguageISO } from '../language';
+export declare enum BiteRequestTypes {
+    create = "create",
+    get = "get"
+}
+export interface PayloadGetBiteRequestV5 {
+    readonly id: string;
+    readonly type: BiteRequestTypes.get;
+}
+interface PayloadBiteBase {
+    readonly creator: string;
+    readonly words: string[];
+    readonly suggestedLanguage?: LanguageISO;
+    readonly onlyOfferSuggestedLanguage?: boolean;
+    readonly disabled?: boolean;
+    readonly theme?: string;
+    readonly backgroundImage?: string;
+}
+export interface PayloadCreateBiteRequestV5 extends PayloadBiteBase {
+    readonly type: BiteRequestTypes.create;
+}
+export declare type PayloadBiteRequestV5 = PayloadCreateBiteRequestV5 | PayloadGetBiteRequestV5;
+export interface PayloadBiteResponseV5 extends PayloadBiteBase {
+    readonly id: string;
+}
+export {};
