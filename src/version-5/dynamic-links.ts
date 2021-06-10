2366d36b00d3e2c09b425408fde9701d5384c24a
lucjac <lucas.jacobson@hotmail.de>
2021-06-10 14:17:40 +0200

    feature(DynamicLinks): add endpoint (WIP) (#226)
    * feature(DynamicLinks): add endpoint
    
    * fix(DL): rename type

---
+++ src/version-5/dynamic-links.ts
@@ -0,0 +1,14 @@
+import { PayloadRequestBaseV5 } from './base'
+
+enum PayloadDynamicLinksRequestType {
+    create = 'sync',
+    refresh = 'refresh',
+}
+
+export interface PayloadDynamicLinksRequestV5 extends PayloadRequestBaseV5 {
+    readonly type: PayloadDynamicLinksRequestType
+}
+
+export interface PayloadDynamicLinksResponseV5 {
+    readonly link: string
+}
