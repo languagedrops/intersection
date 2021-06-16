28dcba91120346e9592bd1c977494a268aef4269
Lucas <lucas.jacobson@hotmail.de>
2021-06-16 14:50:49 +0200

    feature(Connections): add request types

--- src/version-5/sync-connections.ts
+++ src/version-5/sync-connections.ts
@@ -1,3 +1,4 @@
+import { PayloadRequestBaseV5 } from './base'
 
 interface PayloadConnectionRequestV5 {
     readonly connectionUserId: string
@@ -9,10 +10,30 @@ interface PayloadConnectionResponseV5 {
     readonly profilePicLink: string
 }
 
-export interface PayloadSyncConnectionsRequestV5 {
+export enum PayloadConnectionRequestType {
+    sync = 'sync',
+    initiateConnection = 'initiateConnection',
+    removeConnection = 'removeConnection',
+}
+
+interface PayloadConnectionBaseV5<T extends PayloadConnectionRequestType> extends PayloadRequestBaseV5 {
+    readonly type: T
+}
+
+export interface PayloadConnectionsSyncRequestV5 extends PayloadConnectionBaseV5<PayloadConnectionRequestType.sync> {
     readonly connections: PayloadConnectionRequestV5[]
     readonly lastSyncDate: number
 }
+
+export interface PayloadConnectionsInitiateRequestV5 extends PayloadConnectionBaseV5<PayloadConnectionRequestType.initiateConnection> {
+    readonly link: string
+}
+
+export interface PayloadConnectionsRemoveRequestV5 extends PayloadConnectionBaseV5<PayloadConnectionRequestType.removeConnection> {
+    readonly connectionUserId: string
+}
+
+export type PayloadSyncConnectionsRequestV5 = PayloadConnectionsInitiateRequestV5 | PayloadConnectionsSyncRequestV5 | PayloadConnectionsRemoveRequestV5
 
 export interface PayloadSyncConnectionsResponseV5 {
     readonly connections: PayloadConnectionResponseV5[]
