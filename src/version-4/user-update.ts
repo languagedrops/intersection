1b89b25dc33bbf758d0856bc3ac2947f13c0ff54
Lucas <lucas.jacobson@hotmail.de>
2021-07-05 15:59:07 +0200

    feature(User): add displayName

--- src/version-4/user-update.ts
+++ src/version-4/user-update.ts
@@ -14,6 +14,7 @@ interface PayloadUserDetailsBaseV4<T extends PayloadLoginTypeV4> extends Payload
 
 interface PayloadLoggedInUserDetailsBaseV4<T extends PayloadLoginTypeV4> extends PayloadUserDetailsBaseV4<T> {
     readonly email: string
+    readonly displayName?: string
 }
 
 export interface PayloadGoogleUserDetailsV4 extends PayloadLoggedInUserDetailsBaseV4<PayloadLoginTypeV4.Google> {
