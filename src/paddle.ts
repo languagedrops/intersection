e3decd8c2a875073cceb6a9fb60717a611c0c81d
Anton Semenov <anta.semenov@gmail.com>
2021-07-06 15:52:07 +0500

    feature(Paddle): add branbassador id to passthrough
    It's not needed right now, but can be useful in future

--- src/paddle.ts
+++ src/paddle.ts
@@ -3,4 +3,5 @@ export interface PayloadPaddlePassthrough {
     readonly userId: string
     readonly affiliateUserID?: string,
     readonly sscid?: string,
+    readonly brandbassadorId?: string
 }
