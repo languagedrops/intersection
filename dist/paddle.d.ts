42408cdd9da2f169c28552b33113788ef8586c97
Anton Semenov <anta.semenov@gmail.com>
2021-07-06 16:50:12 +0500

    chore(Project): generate JS files

--- dist/paddle.d.ts
+++ dist/paddle.d.ts
@@ -2,4 +2,5 @@ export interface PayloadPaddlePassthrough {
     readonly userId: string;
     readonly affiliateUserID?: string;
     readonly sscid?: string;
+    readonly brandbassadorId?: string;
 }
