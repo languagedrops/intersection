0c2a772510d131220b1aaa3364eaf40533b671b0
lucjac <lucas.jacobson@hotmail.de>
2021-07-07 14:27:34 +0200

    feature(UploadProfileImage): add endpoint (#244)
    * feature(UploadProfileImage): add endpoint
    
    * fix(UploadProfileImage): request name

---
+++ src/version-5/upload-profile-image.ts
@@ -0,0 +1,8 @@
+
+export interface PayloadUploadProfileImageRequestV5 {
+    readonly base64EncodedImageString: string
+}
+
+export interface PayloadUploadProfileImageResponseV5 {
+    readonly imageUrl: string
+}
